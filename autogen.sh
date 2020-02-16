9e22a7f5d4cd942500af8d0c255e082c51492019
Romain Bezut <morian@xdec.net>
2020-02-17 00:40:18 +0100

    gitlab: fix failing CI on centos8, add centos7 support.

--- autogen.sh
+++ autogen.sh
@@ -5,5 +5,5 @@ unset LC_ALL
 export LC_COLLATE=C
 export LC_NUMERIC=C
 
-autoreconf --install --symlink
-rm --recursive --force autom4te.cache
+autoreconf --force --install --symlink
+rm --recursive --force autom4te*.cache
